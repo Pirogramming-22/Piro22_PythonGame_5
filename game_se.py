a018f00498084f30b8b2606aac1cae7727f26b5d
iseun15 <ave0315@naver.com>
2025-01-05 09:10:00 +0900

    [feat] 아파트게임 기능 구현

---
+++ game_se.py
@@ -0,0 +1,54 @@
+#5번 아파트게임
+import random
+
+#기본 정보 *수정*
+my_turn='하연'   #일단 플레리어를 하연으로 설정 *수정*
+num_player=3;   #일단 3명이 게임한다고 가정 *수정*
+players=['하연', '은서','연서']   #*수정*
+
+
+def apt_game(my_turn, num_player, players) :
+    #인트로
+    print("아파트~ 아파트~ 아파트~ 아파트~")  #인트로
+    #층수제한
+    limit_floors = (num_player*2)*5
+    #층수 입력
+    while True :
+        target_floor = int(input(f'{my_turn} : ')) 
+
+        if target_floor > limit_floors :
+            print(f'빠른 진행을 위해 {limit_floors}층 안에서 골라주세요~')
+        elif target_floor<=num_player*2 :
+            print(f'노잼~ 더 높은 층으로~')
+        else:
+            break
+
+    #각 플레이어에게 층수 부여 (전체 층수에서 나머지로 당첨자 선정)
+    base_floor = list(range(1, (num_player*2)+1))
+    random.shuffle(base_floor) 
+    player_hands = {player : [] for player in players}
+    for i, floor in enumerate(base_floor):
+        player = players[i%num_player]
+        player_hands[player].append(floor)
+
+
+    for player, hand in player_hands.items():
+            print(f"{player}: {hand}") 
+
+
+
+    calc_floor = (target_floor%(num_player*2))
+    if calc_floor == 0:
+        calc_floor = num_player*2
+    for player, hand in player_hands.items():
+        if calc_floor in hand:
+            print(f"{player}~ 원샷!")
+            #또는 return player
+
+
+
+
+
+
+
+apt_game(my_turn, num_player, players)
